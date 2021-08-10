62aeccebb927dfa7d30b117d2e6ffd3a87ee6a7a
Nosov-Pavel <nopaev@gmail.com>
2021-08-10 07:10:07 -0400

    namesId

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 function App() {
 
-    const names = ['ALice', 'Bob', 'Jim', 'Zoe'];
+    const names = [
+        {
+            id: '1',
+            title: 'ALice',
+            phone: '777-333-77-33'
+        },
+        {
+            id: '2',
+            title: 'Bob',
+            phone: '777-333-77-37'
+
+        }, 'Jim', 'Zoe'];
 
 
     return (
         <div>
             {names}
             <ul>
-                {names.map(el =><li>{el}</li>)}
+                {names.map(el => <li key={el}>{el}</li>)}
             </ul>
         </div>
 
