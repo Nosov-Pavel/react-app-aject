6d7bf00ef3f42cf1125e90341401508e982328c6
Nosov-Pavel <nopaev@gmail.com>
2021-08-10 07:14:05 -0400

    namesIdEl

--- src/App.js
+++ src/App.js
@@ -10,15 +10,23 @@ function App() {
             id: '2',
             title: 'Bob',
             phone: '777-333-77-37'
-
-        }, 'Jim', 'Zoe'];
+        },
+        {
+            id: '3',
+            title: 'Zoe',
+            phone: '777-333-77-73'
+        },
+        {
+            id: '4',
+            title: 'Jim',
+            phone: '777-333-37-33'
+        }];
 
 
     return (
         <div>
-            {names}
             <ul>
-                {names.map(el => <li key={el}>{el}</li>)}
+                {names.map(el => <li key={el.id}>{el.title} {el.phone}</li>)}
             </ul>
         </div>
 
