538e08ee4cc08ca2299b9cf725eb8ee02c090da1
Nosov-Pavel <nopaev@gmail.com>
2021-08-09 22:10:53 -0400

    button+1

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import React, {useState} from "react";
 function App() {
     const [counter, setCounter] = useState(5);
 
-    // const buttonPlus => () => {
-    //     setCounter(co)
-    // }
+    const buttonPlus = () => {
+        setCounter(counter +1)
+    }
 
 
     return (
         <div>
             {counter}
+            <button onClick={buttonPlus}>+</button>
         </div>
     )
 
