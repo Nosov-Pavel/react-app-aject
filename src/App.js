04cb7e6a74adf72d6271453368734b389cde4907
Nosov-Pavel <nopaev@gmail.com>
2021-08-09 21:50:26 -0400

    setC

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
 import React, {useState} from "react";
 
 function App() {
-
-    const [counter, setCounter] = useState(5);
-
-
-
-    const buttonPlus = () => {
-        setCounter(counter +1)
+    const [counter, setCounter] = useState(10);
 }
-    const buttonMinus = () => {
-        setCounter(counter -1)
-    }
-
-    return(
-        <div>
-            <button onClick={buttonMinus}>-</button>
-            {counter}
-            <button onClick={buttonPlus}>+</button>
-        </div>
-    )
-}
-
-export default App;
