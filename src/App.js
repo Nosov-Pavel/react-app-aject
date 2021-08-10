87dac134a9c107f756dc7be3fcf975e304272d04
Nosov-Pavel <nopaev@gmail.com>
2021-08-09 22:07:20 -0400

    just5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import React, {useState} from "react";
 
 function App() {
-    const [counter, setCounter] = useState(10);
+    const [counter, setCounter] = useState(5);
+
+    // const buttonPlus => () => {
+    //     setCounter(co)
+    // }
+
+
+    return (
+        <div>
+            {counter}
+        </div>
+    )
+
 }
+
+    export default App;
