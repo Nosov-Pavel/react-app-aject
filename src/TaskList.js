5bffb424dc05ad664016d9817a5796b04762e60b
Nosov-Pavel <nopaev@gmail.com>
2021-08-11 07:08:46 -0400

    props.v in task

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,3 +1,5 @@
+import TopMenu from "./TopMenu";
+
 function TaskList(props){
     console.log(props)
     return(
@@ -5,6 +7,8 @@ function TaskList(props){
             <li>{props.siteName}</li>
             <li>{props.veris}</li>
             <li>Task 3</li>
+
+            <TopMenu version={props.veris} />
         </ul>
     )
 }
