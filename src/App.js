b90da777625dc07f7807855489dbcaf143a53049
Nosov-Pavel <nopaev@gmail.com>
2021-08-12 13:56:58 -0400

    lN

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import TopMenu from "./TopMenu";
 
 function App(){
     const name = 'Counter Application';
-    const version = 'v1.2.3'
+    const version = 'v1.2.3';
+    const lastName = 'N.L.M'
 
     
     return(
         <div>
             <TopMenu v={version}/>
             <TaskList siteName={name} version={version}/>
+            <TopMenu lN={lastName}/>
 
         </div>
     );
