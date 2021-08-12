e696328fe5a16b62df196b2179c0f72d6ca2c3fc
Nosov-Pavel <nopaev@gmail.com>
2021-08-11 22:22:11 -0400

    M1.1

---
+++ src/Menu 1.1.js
@@ -0,0 +1,10 @@
+import Menu1 from "./Menu 1";
+
+function Menu11(props){
+    return(
+        <div>
+            <Menu1 fromMenu1={props.fromM1}/>
+        </div>
+    )
+}
+export default Menu11;
--- src/Menu 1.js
+++ src/Menu 1.js
@@ -1,6 +1,13 @@
+import Menu11 from "./Menu 1.1";
+
 function Menu1(props){
+
+    const fromMenu1 = 'secondLevel1';
     return(
-        <div>Names{props.fromApp}</div>
+        <div>
+            Names{props.fromApp}
+            <Menu11 fromM1={fromMenu1}/>
+        </div>
 
     )
 }
