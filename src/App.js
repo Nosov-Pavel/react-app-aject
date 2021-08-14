2665e88b898a4d119a74e4433cf4835108efb81d
Nosov-Pavel <nopaev@gmail.com>
2021-08-13 22:56:41 -0400

    store

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import TaskList from "./TaskList";
-import TopMenu from "./TopMenu";
 
 function App(){
-    const name = 'Counter Application';
-    const version = 'v1.2.3';
-    const lastName = 'N.L.M'
+
+    const storeName = 'I am How I am'
+
 
     
     return(
         <div>
-            <TopMenu v={version}/>
-            <TaskList siteName={name} version={version}/>
-            <TopMenu lN={lastName}/>
+            <h1>{storeName}</h1>
 
         </div>
     );
