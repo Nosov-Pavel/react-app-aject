bc398e9373e96c94db8752a0ea2829a3126f0075
Nosov-Pavel <nopaev@gmail.com>
2021-08-15 08:30:14 -0400

    Footer[]

--- src/Footer.js
+++ src/Footer.js
@@ -1,9 +1,13 @@
 function Footer(){
+    const footerMenu = ['contacts', 'address', 'Facebook', 'Privacy policy', 'Delivery' ];
     return(
-        <div>
-
-
-        </div>
+      <ul>
+          <li>{footerMenu[0]}</li>
+          <li>{footerMenu[1]}</li>
+          <li>{footerMenu[2]}</li>
+          <li>{footerMenu[3]}</li>
+          <li>{footerMenu[4]}</li>
+      </ul>
     );
 }
 export default Footer;
